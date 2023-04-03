f44faea46cfb9d456eaa2f78d1df89a178e88420
JamesB0907 <virverum22@gmail.com>
2023-04-03 13:47:57 -0400

    WILL IT WORK? Task module hopefully function w/o css

--- src/scripts/messages.js
+++ src/scripts/messages.js
@@ -1,5 +1,5 @@
 // Responsibility: Generate the HTML for chat and new message section
-import { deleteMessage, getMessages, saveMessage } from "./dataAccess.js"
+import { deleteMessage, getMessages, saveMessage, thumbsDownMessage, thumbsUpMessage } from "./dataAccess.js"
 
 export const createMessageModule = () => {
 const messages = getMessages()
@@ -10,7 +10,13 @@ return `<h3>Chat</h3>
                 return `<div class="message-container">
                             <div class="message-name">${message.userName}:</div>
                             <div class="message-text-container"><p class="message-text">${message.message}</p></div> 
-                            <button class="image-delete-button" id="delete-message--${message.id}" value="${message.id}">Delete</button>
+                            <div class="message-interactions">
+                                <img src="./images/thumbsUp.png" class="thumb" id="thumbsUp-${message.id}">
+                                <div class="thumb-count">${message.thumbsUp}</div>
+                                <img src="./images/thumbsDown.png" class="thumb" id="thumbsDown-${message.id}">
+                                <div class="thumb-count">${message.thumbsDown}</div>
+                                <button class="delete-button" id="delete-message--${message.id}" value="${message.id}">Delete</button>
+                            </div>
                         </div>`}).join("")
             }
         </div>
@@ -23,6 +29,22 @@ return `<h3>Chat</h3>
             <button class="button" id="new-message-save-button">Send</button>
         </div>`
 }
+
+// listen for the click of the  THUMBS UP button and call the function that increases that count when clicked
+document.addEventListener("click", event => {
+    if (event.target.id.startsWith("thumbsUp-")) {
+        const [,messageID] = event.target.id.split("-")
+        thumbsUpMessage(parseInt(messageID))
+    }
+})
+
+// listen for the click of the THUMBS DOWN button and call the function that increases that count when clicked
+document.addEventListener("click", event => {
+    if (event.target.id.startsWith("thumbsDown-")) {
+        const [,messageID] = event.target.id.split("-")
+        thumbsDownMessage(parseInt(messageID))
+    }
+})
 
 
 // listen for the click of the message delete button and delete the image when clicked
@@ -51,7 +73,9 @@ document.querySelector("#dashboard").addEventListener("click", clickEvent => {
         else{
             const newMessageObject = {
                 userName: userName,
-                message: userMessage
+                message: userMessage,
+                thumbsUp: 0,
+                thumbsDown: 0
             }
 
             // send the message object to the API for permanent storage
