3fd40cebdb4ccced6169086ef55b0450abbc1582
gabby <gabbyschryver@gmail.com>
2023-04-04 08:09:23 -0700

    changes

--- src/scripts/createHTML.js
+++ src/scripts/createHTML.js
@@ -1,18 +1,19 @@
-// main.js or any other file where you define createHTML()
-
 import { listOfEvents } from "./Events.js"
 import { createImageModule } from "./images.js"
 import { createMessageModule } from "./messages.js"
 import { createTaskBox } from "./tasks.js"
 import { Articles } from "./articles.js"
 
-export const createHTML = factHTML => {
-  return `
-    ${createImageModule()}
-    ${createMessageModule()}
-    ${listOfEvents()}
-    ${createTaskBox()}
-    ${Articles()}
-    ${factHTML}
-  `;
-};
+
+export const createHTML = () => {
+      // Render all your UI components here
+      return `
+     
+      ${createImageModule()}
+      ${createMessageModule()}
+      ${listOfEvents()}
+      ${createTaskBox()}
+      ${Articles()}
+
+      `
+}
