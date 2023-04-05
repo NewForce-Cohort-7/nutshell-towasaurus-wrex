5a0d223a9273434ec9d39951b25319553644b232
guacajoely <guacajoely@gmail.com>
2023-04-05 09:02:16 -0400

    added conditional for daily limit exceeded

--- src/scripts/dadJokes.js
+++ src/scripts/dadJokes.js
@@ -3,11 +3,16 @@ import { fetchRandomJoke, getDadJoke, setDadJoke } from "./dataAccess.js"
 // Create HTML for Dad Joke Section 
 export const createDadJokes = () => {
     const dadJoke = getDadJoke()
-    return `<div id="dad-joke">
-                <h2>Random Dad Joke</h2>
-                <p>${dadJoke.body[0].setup}</br>${dadJoke.body[0].punchline}</p>
-                <button id="new-joke">Give me another joke</button>
-            </div>`
+
+    if(dadJoke.message){return `<div id="dad-joke">Max Dad Jokes Exceeded (50)</div>`}
+
+    else{
+        return `<div id="dad-joke">
+                    <h2>Random Dad Joke</h2>
+                    <p>${dadJoke.body[0].setup}</br>${dadJoke.body[0].punchline}</p>
+                    <button id="new-joke">Give me another joke</button>
+                </div>`
+    }
 }
 
 // Event listener for the "Generate Fact" button click event
