2c040e13cfd309c5d8783e1b3d6efcb0e222467e
gabby <gabbyschryver@gmail.com>
2023-04-05 08:31:45 -0700

    new main

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,8 @@
 
-import { fetchArticles, fetchEvents, fetchMessages, fetchPhotos, fetchTasks, getChuckFact, setChuckFact, fetchRandomJoke } from "./dataAccess.js"
+import { fetchArticles, fetchEvents, fetchMessages, fetchPhotos, fetchTasks, setChuckFact, fetchRandomJoke } from "./dataAccess.js"
 import { createHTML } from "./createHTML.js"
 import { fetchRandomFact } from "./apiAccess.js"
+import { initEventListeners } from "./articles.js"
 
 const dashboard = document.querySelector("#dashboard")
 
@@ -12,18 +13,20 @@ const render = () => {
         .then(() => fetchPhotos())
         .then(() => fetchMessages())
         .then(() => fetchRandomJoke())
-        .then(() => fetchRandomFact())
-        .then(fact => {
-            setChuckFact(fact)
-        })
         .then(() => {
             dashboard.innerHTML = createHTML()
+            // Initialize event listeners for the Articles component
+            initEventListeners()
+            // Fetch a random Chuck Norris fact
+            return fetchRandomFact()
+        })
+        // Set the fact in the application state
+        .then(fact => {
+            setChuckFact(fact)
         })
 }
 
 render()
-
-
 
 dashboard.addEventListener("stateChanged", customEvent => {
     render()
